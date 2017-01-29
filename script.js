e841f88922ec0a7103b9bbb1c0a37bdcab3b4326
Maciek W <maciej.wmroblewski@gmail.com>
2017-01-29 23:40:55 +0100

    poprawki

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-// postanowiłem opisac linijke po linijce kod. Mozec coś xle zrozumialem
 
 
-var url = 'https://restcountries.eu/rest/v1/name/'; // przypisuje do zmienej adres url z którego na koncu po dodaniu nazwy kraju bede w tablicy której będe mogł pobierać dane o kraju wyszukiwania
-var countriesList = $("#countries"); //tutaj za pomoc JQ pobieram listę krajów jakie znajdu się w ul z id="countries"
+
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries'); 
 
 $('#search').click(searchCountries); 
-/*Tu za pomoca JQ wyszukuje element w html który my id "search" czyli nasz butto i nasłuchuje czy jest on kliknięty. Jeśli zostanie kliknięty wlaczy się funkcje searchCountries. Swoja droga czemu po searchCountries nie daje () skoro to funkcja*/
 
-function searchCuntries() { //definiujemy funkcje
-	var countryName = $('#country-name').val(); // pobieramy  do zmiennej nazwe państwa która ktoś wpisal w input
-	if (!countryName.length) countryName = "Poland"; //sprawdzamy czy ktoś cos wpisal do wyszukiwania. Jeśli nic nie wpisał a nacisnł button to przypisujemy mu Poland
 
-	$.ajax({// JQ zapytanie ajacowe
-		url: url + countryName, //linkujemy do kraju do którego ktoś chce się dowiedzieć więcej informacji
-		method: 'GET', //pobieramy dane o wyszukiwanym kraju
-		success: showCountriesList //jeżeli uda nam się pobrać info uruchamia się ta funkcja która ma za zadanie pokazać nam listę krajów. Znów nie kumam czemu, skoro to funkcja nie dajemy nawiasów i nie wpisujemy (resp)
+function searchCountries() {
+	var countryName = $('#country-name').val(); 
+	if (!countryName.length) countryName = "Poland";
+
+	$.ajax({
+		url: url + countryName, 
+		method: 'GET', 
+		success: showCountriesList 
 	});
 }
 
-function showContriesList(resp){ //definiujemy funkcje. Jeszcze nie wiem jak bedzie użyty resp. Zakładam że ta zmienne będzie zmieniał jsona tak bym mógł nim manipulować w JS
-	countriesList.empty(); //Czyścimy listę z starych wyszukiwań
+function showCountriesList(resp){ 
+	countriesList.empty(); 
+
+	resp.forEach(function(item) {
+		$("<li>").text(item.name).appendTo(countriesList)
+	})
 
 }
 
-/* brakuje nam jeszcze w fukcji przjęcia danych z API o wyszukiwaych krajach oraz włożenie ich do ul o id "countries"*/
